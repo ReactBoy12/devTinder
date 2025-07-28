b0ce399763a281aed2e370f2ed1bc74d1db0fe7c
shailesh gore <shaileshgore1998@gmail.com>
2025-07-28 21:28:10 +0530

    used the model 2 and used them in the code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const User2Model = require("./models/user2");
 const app = express();
 
 app.post("/signUp", async (req, res) => {
@@ -19,6 +20,34 @@ app.post("/signUp", async (req, res) => {
     res.status(401).send("user registered successfully");
   } catch (err) {
     res.status(500).send("something went wrong => ", err);
+  }
+});
+
+// app.post("/newSignUp", async (req, res) => {
+//   try {
+//     const data = {
+//       email: "shailesh@gore.com",
+//       password: "Shailesh@123",
+//       mobileNumber: "9876543210",
+//     };
+
+//     const newsignup = new User2Model(data);
+//     await newsignup.save();
+//     res.status(201).send("Entry created...");
+//   } catch (err) {
+//     res.status(501).send("Error generated.....", err);
+//   }
+// });
+
+app.get("/getAllUser", async (req, res) => {
+  try {
+    const persons = await User2Model.find({});
+    res.status(200).send(persons);
+    if (!res.status(200)) {
+      throw new Error("error while getting the details..");
+    }
+  } catch (err) {
+    res.status(401).send(err);
   }
 });
 
