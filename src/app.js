0d2b20cb181adcf2e24ac68cd8f54eef2678e3c7
shailesh gore <shaileshgore1998@gmail.com>
2025-07-29 11:28:47 +0530

    query through model of finding

--- src/app.js
+++ src/app.js
@@ -3,17 +3,11 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const User2Model = require("./models/user2");
 const app = express();
+app.use(express.json());
 
 app.post("/signUp", async (req, res) => {
   try {
-    const userObj = {
-      firstName: "krishna",
-      lastName: "yadav",
-      emailId: "krishna@yadav.com",
-      password: "krishna@123",
-      age: 22,
-      gender: "male",
-    };
+    let userObj = req.body;
     const newUser = new User(userObj);
     await newUser.save();
     // throw new Error("sdsd");
@@ -23,31 +17,47 @@ app.post("/signUp", async (req, res) => {
   }
 });
 
-// app.post("/newSignUp", async (req, res) => {
-//   try {
-//     const data = {
-//       email: "shailesh@gore.com",
-//       password: "Shailesh@123",
-//       mobileNumber: "9876543210",
-//     };
-
-//     const newsignup = new User2Model(data);
-//     await newsignup.save();
-//     res.status(201).send("Entry created...");
-//   } catch (err) {
-//     res.status(501).send("Error generated.....", err);
-//   }
-// });
-
-app.get("/getAllUser", async (req, res) => {
+app.get("/user", async (req, res) => {
   try {
-    const persons = await User2Model.find({});
-    res.status(200).send(persons);
-    if (!res.status(200)) {
-      throw new Error("error while getting the details..");
+    let userEmail = req.body.emailId;
+    const userDetails = await User.findOne({ emailId: userEmail });
+    if (userDetails.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.status(200).send(userDetails);
     }
   } catch (err) {
-    res.status(401).send(err);
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/userById", async (req, res) => {
+  try {
+    let userId = req.body._id;
+    console.log("details", typeof userId);
+    const userDetails = await User.findById(userId).exec();
+
+    if (userDetails.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.status(200).send(userDetails);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+
+    if (allUsers.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.status(200).send(allUsers);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
   }
 });
 
